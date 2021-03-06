acd7093a484dc3ce674fad663834017e8d4b9dc3
Gradio2000 <laskin999@gmail.com>
2021-03-06 14:39:43 +0500

    HW5_2

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -3,9 +3,13 @@ package ru.javawebinar.topjava.repository.datajpa;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 public class DataJpaMealRepository implements MealRepository {
@@ -21,23 +25,46 @@ public class DataJpaMealRepository implements MealRepository {
         return null;
     }
 
+    @Transactional
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        Meal meal = get(id, userId);
+        if (meal.getUser().id() != userId){
+            return false;
+        }
+        else {
+            crudRepository.delete(meal);
+            return true;
+        }
     }
 
+    @Transactional
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal meal = crudRepository.findById(id).orElse(null);
+        if (meal == null || meal.getUser() == null ||  meal.getUser().id() != userId){
+            throw new NotFoundException("aaa");
+        }
+        return meal;
     }
 
+    @Transactional
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.findAll().stream()
+                .filter(meal -> meal.getUser().id() == userId)
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .collect(Collectors.toList());
     }
 
+    @Transactional
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return crudRepository.findAll().stream()
+                .filter(meal -> meal.getUser().id() == userId)
+                .filter(meal -> meal.getDateTime().isAfter(startDateTime))
+                .filter(meal -> meal.getDateTime().isBefore(endDateTime))
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .collect(Collectors.toList());
     }
 }
